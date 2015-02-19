1263fd3fa564c681e4d053720acb34fbe532ff0a
Francis Bautista <francis.bautista07@gmail.com>
2015-02-19 16:18:41 +0800

    created test data

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var d3 = require("d3");
-var dataset = []
-d3.csv("collated_status_20140725.csv", function(data) {
-   dataset = data.map(function(d) { return [ +d["statusN"], +d["statusS"] ]; });
+
+var test_data = ["Saab", "Volvo", "BMW"];
+
+d3.csv("data/daily_status/collated_status_20140725.csv", function(data) {
+   dataset = data.map(function(d) { return +d["year"], +d["month"],[ +d["day"], +d["hour"], +d["lineID"], +d["stationID"], +d["statusN"], +d["statusS"] ]; });
+   console.log(dataset)
 });
-console.log(dataset)
