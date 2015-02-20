307fee09db5fec375d9246936c1b43a8cb265e5c
Francis Bautista <francis.bautista07@gmail.com>
2015-02-20 21:25:40 +0800

    added documentation

--- scripts/selection_engine.py
+++ scripts/selection_engine.py
@@ -36,16 +36,20 @@ def station_writer(num,year,month,day,hour,lineID,stationID,statusN,statusS, loo
 
 
 def station_isolation_engine():
+	# Map CSV cols to labels for manipulation
 	year,month,day,hour,lineID,stationID,statusN,statusS = loadtxt(FN_LINE, usecols =(0,1,2,3,4,5,6,7,), unpack=True, delimiter=",", dtype=int, skiprows = 1)
 	ctr = 0
+
+	# Loop through each item of the file
 	for i in arange(len(year)):
 		ctr = ctr + 1
 
-		# TODO: Fix loop here. Only station 4 is being read
-		for j in arange(19):
-			if stationID[i]==j: # if stationID[0]==0
-				station_writer(j, year[i],month[i],day[i],hour[i],lineID[i],stationID[i],statusN[i],statusS[i], i)
+		# Loop through each station and check if it matches with current station
+		for current_station in arange(19):
+			if stationID[i]==current_station: # if stationID[0]==0
+				station_writer(current_station, year[i],month[i],day[i],hour[i],lineID[i],stationID[i],statusN[i],statusS[i], i)
 
+		# Progress Bar
 		percentage = ctr/float(len(year))*100
 		sys.stdout.write("\r")
 		progress = ""
@@ -65,15 +69,19 @@ def line_selection_engine(line_number):
 	fn_out= FN_LINE
 	fp=open(fn_out,'w')
 	fp.write(OUT_HEADER)
+	# Loop through every filename in the list
 	for fn in NAME_LIST[0:(len(NAME_LIST)-1)]:
 		ctr = ctr + 1
 
-		# Assign column names to filename cols
+		# Assign column names to CSV cols
 		year,month,day,hour,lineID,stationID,statusN,statusS = loadtxt(fn, usecols = (0,1,2,3,5,6,7,8,), unpack=True, delimiter=",", dtype=int, skiprows = 1)
 
+		# Loop through every element in each file and store
 		for i in arange(len(year)):
 			if(lineID[i]==line_number):
 				fp.write("%d,%d,%d,%d,%2d,%3d,%d,%d\n" % (year[i],month[i],day[i],hour[i],lineID[i],stationID[i],statusN[i],statusS[i]))
+
+		# Progress Bar
 		total_percentage = (ctr/float(len(NAME_LIST)))*100
 		current_file =" Currently reading \""+ fn + "\""
 		bar_length=100
