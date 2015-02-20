238a1e93f3a4704ce74137a858757e180c752a73
Francis Bautista <francis.bautista07@gmail.com>
2015-02-20 09:56:11 +0800

    Finalized print status engine

--- scripts/selection_engine.py
+++ scripts/selection_engine.py
@@ -11,13 +11,14 @@ OUT_HEADER = "#year,month,day,hour,qtr,lineID,stationID,statusN,statusS\n"
 NAME_LIST = [DIR_PREFIX + s  for s in loadtxt(INPUT_FILE, dtype='a',unpack=False)]
 
 def main():
-
+	ctr = 0
 	# Loop through each filename
 	for fn in NAME_LIST[0:(len(NAME_LIST)-1)]:
+		ctr = ctr + 1
 		fn_out= "../output/collated_status.csv"
 		fp=open(fn_out,'w')
 		fp.write(OUT_HEADER)
-		
+
 		# Assign column names to filename cols
 		year,month,day,hour,lineID,stationID,statusN,statusS = loadtxt(fn, usecols = (0,1,2,3,5,6,7,8,), unpack=True, delimiter=",", dtype=int, skiprows = 1)
 		## MAKE ANOTHER FUNCTION TO PARSE BIGASS FILE
@@ -25,7 +26,9 @@ def main():
 		for i in arange(len(year)):
 			if(lineID[i]==4):
 				fp.write("%d,%d,%d,%d,%2d,%3d,%d,%d\n" % (year[i],month[i],day[i],hour[i],lineID[i],stationID[i],statusN[i],statusS[i]))
-				sys.stdout.write("#")
+				total_percentage = (ctr/float(len(NAME_LIST)))*100
+				current_file =" Currently reading \""+ fn + "\""
+				sys.stdout.write(" of" + " Total. " + current_file+ "\r%d%%" % total_percentage)
 				sys.stdout.flush()
 		fp.close()
 
