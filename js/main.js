a255911d917101543662512b75c8ff83a0ce8451
Eyana <eyana.mallari@obf.ateneo.edu>
2015-02-25 23:50:23 +0800

    modified selectors and legend:

--- js/main.js
+++ js/main.js
@@ -14,19 +14,41 @@ var end = moment().endOf('day').fromNow();
 
 
 $('select[name=line_select]').change(function() {
-	var val = this.value;
-	$("span.line_name").html(val).show();
-	//$("span.line_name").fadeIn("slow");
-	$($('select[name=station_select]').removeAttr('disabled')).focus();
+
+    if (this.value !==  '') {
+		$("span.line_name").html(this.value).show();
+		$("span.line_name").css("color", "#222");
+		$("span.line_name").css("background-color", "rgba(141, 142, 142, 0.5)");
+		//$("span.line_name").fadeIn("slow");	
+		$($('select[name=station_select]').removeAttr('disabled')).focus();	
+    }
+    else {
+        $("span.line_name").hide();
+        $('select[name=station_select]').attr('disabled', 'disabled');
+    }
+
+        
+	
+
 });
 
 $('select[name=station_select]').change(function() {
-	var val = this.value;
-	$("span.station_name").html(val).show();
+    if (this.value != '') {
+		$("span.station_name").html(this.value).show;
+		$("span.station_name").css("color", "#222");
+		$("span.station_name").css("background-color", "rgba(141, 142, 142, 0.5)");
+		//$("span.line_name").fadeIn("slow");		
+    }
+    else {
+        $("span.station_name").hide();
+
+    }
 
 });
 
 $('button').click(function() {
 	var val = $(this).data('label');
 	$("span.when").html(val).show();
+	$("span.when").css("color", "#222");
+	$("span.when").css("background-color", "rgba(141, 142, 142, 0.5)");
 });
