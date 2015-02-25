c7067115a12bac23933ca8462c4170531bf03991
Eyana <eyana.mallari@obf.ateneo.edu>
2015-02-25 17:20:18 +0800

    selectors of location and time

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ d3.select('#slider7').call(d3.slider().axis(true).min(0).max(24).step(1));
 
 
 //d3.select('#slider7').call(d3.slider().axis(true).min(0).max(10).step(1));
+
+
+$('select[name=line_select]').change(function() {
+	var val = this.value;
+	$("span.line_name").html(val).show();
+	//$("span.line_name").fadeIn("slow");
+	$($('select[name=station_select]').removeAttr('disabled')).focus();
+});
+
+$('select[name=station_select]').change(function() {
+	var val = this.value;
+	$("span.station_name").html(val).show();
+
+});
+
+$('button').click(function() {
+	var val = $(this).data('label');
+	$("span.when").html(val).show();
+});
